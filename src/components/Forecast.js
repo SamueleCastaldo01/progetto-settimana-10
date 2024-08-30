7742a0403a6a9a648637a7af5d8a9961871218c7
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-08-30 13:13:49 +0200

    commit

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -6,16 +6,16 @@ function Forecast(props) {
         <h2>Previsioni Oggi: {props.forecast.city.name}</h2>
         <div className="mt-3 row justify-content-center d-flex fw-bold">
           <div className="col-1">Orario</div>
-          <div className="col-3">Descrizione</div>
-          <div className="col-1">Temp</div>
+          <div className="col-2">Descrizione</div>
+          <div className="col-2">Temp</div>
           <div className="col-2">Min Temp</div>
           <div className="col-2">Max Temp</div>
-          <div className="col-1">Umidità</div>
+          <div className="col-2">Umidità</div>
           <div className="col-1">Vento</div>
         </div>
         <div className="row weather-row d-flex justify-content-center align-items-center">
               <div className="col-1">Attuale</div>
-              <div className="col-3">
+              <div className="col-2">
                 <img
                 className="weather-img"
                 src={`http://openweathermap.org/img/wn/${props.weather.weather[0].icon}.png`}
@@ -23,10 +23,10 @@ function Forecast(props) {
                 />
                 {props.weather.weather[0].description}
               </div>
-              <div className="col-1">{Math.round(props.weather.main.temp - 273.15)}°C</div>
+              <div className="col-2">{Math.round(props.weather.main.temp - 273.15)}°C</div>
               <div className="col-2">{Math.round(props.weather.main.temp_min - 273.15)}°C</div>
               <div className="col-2">{Math.round(props.weather.main.temp_max - 273.15)}°C</div>
-              <div className="col-1">{props.weather.main.humidity}%</div>
+              <div className="col-2">{props.weather.main.humidity}%</div>
               <div className="col-1">{props.weather.wind.speed} m/s</div>
             </div>
         {props.forecast.list.map((item, index) => {
@@ -34,17 +34,17 @@ function Forecast(props) {
           return (
             <div className="row weather-row d-flex justify-content-center align-items-center" key={index}>
               <div className="col-1">{time}</div>
-              <div className="col-3">
+              <div className="col-2">
                 <img
                   src={`http://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
                   alt={item.weather[0].description}
                 />
                 {item.weather[0].description}
               </div>
-              <div className="col-1">{Math.round(item.main.temp - 273.15)}°C</div>
+              <div className="col-2">{Math.round(item.main.temp - 273.15)}°C</div>
               <div className="col-2">{Math.round(item.main.temp_min - 273.15)}°C</div>
               <div className="col-2">{Math.round(item.main.temp_max - 273.15)}°C</div>
-              <div className="col-1">{item.main.humidity}%</div>
+              <div className="col-2">{item.main.humidity}%</div>
               <div className="col-1">{item.wind.speed} m/s</div>
             </div>
           );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,11 +6,13 @@ import CircularProgress from "@mui/material/CircularProgress";
 import SearchIcon from "@mui/icons-material/Search";
 import Forecast from "../components/Forecast";
 import Weather from "../components/Weather";
+import moment from "moment";
+import "moment/locale/it"; // Importa la localizzazione italiana
 
 function HomePage() {
   const [forecast, setForecast] = useState(null);
   const [weather, setWeather] = useState(null);
-  const [days, setDays] = useState(null);
+  const [daysWeather, setDaysWeather] = useState(null);
   const color = "#1A4899";
   const [inpSearch, setInpSearch] = useState("");
   const [isLoading, setIsLoading] = useState(false);
@@ -19,9 +21,7 @@ function HomePage() {
   //-----------------------------------------------------------------------------------
   const fetchForecast = (city) => {
     const apiKey = "7ea4fef49e63c77f69aecc239adf4b1b";
-    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${encodeURIComponent(
-      city
-    )}&appid=${apiKey}`;
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
 
     setIsLoading(true);
     setIsError(false);
@@ -61,9 +61,7 @@ function HomePage() {
   //----------------------------------------------------------------------------------------------
   const fetchWeather = (city) => {
     const apiKey = "7ea4fef49e63c77f69aecc239adf4b1b";
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(
-      city
-    )}&appid=${apiKey}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
     setIsLoading(true);
     setIsError(false);
@@ -87,11 +85,45 @@ function HomePage() {
       });
   };
 
+  //----------------------------------------------------------------------------------------------
+  const fetchDays = (city) => {
+    const apiKey = "7ea4fef49e63c77f69aecc239adf4b1b";
+    const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+
+    setIsLoading(true);
+    setIsError(false);
+
+    fetch(url)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("La chiamata non è andata a buon fine");
+        }
+      })
+      .then((data) => {
+        // Filtra i dati per ottenere solo le previsioni giornaliere
+        const dailyForecast = data.list.filter((item) =>
+          item.dt_txt.includes("12:00:00")
+        );
+        setDaysWeather(dailyForecast.slice(0, 5)); // Prendi solo i primi 5 giorni
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.error("ERRORE NEL RECUPERO DATI (internet)?", err);
+        setIsLoading(false);
+        setIsError(true);
+      });
+  };
+
+  //-------------------------------------------------
+  //submit si attiva quando premo il pulsante di ricerca
   function handleSubmit(e) {
     e.preventDefault();
     if (inpSearch.trim()) {
       fetchWeather(inpSearch.trim());
       fetchForecast(inpSearch.trim());
+      fetchDays(inpSearch.trim());
     }
   }
 
@@ -109,12 +141,14 @@ function HomePage() {
             variant="outlined"
             onChange={(event) => setInpSearch(event.target.value)}
             value={inpSearch}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <SearchIcon />
-                </InputAdornment>
-              ),
+            slotProps={{
+              input: {
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              },
             }}
           />
           <Button
@@ -126,15 +160,58 @@ function HomePage() {
           </Button>
         </form>
 
-        {isLoading && (
-            <CircularProgress />
-        )}
+        {isLoading && <CircularProgress />}
         {isError && <p>Errore nel recupero dei dati</p>}
-        {weather && (
-            <Weather  weather={weather}/>
-        )}
+        {weather && <Weather weather={weather} />}
         {forecast && weather && (
           <Forecast forecast={forecast} weather={weather} />
+        )}
+        {daysWeather && (
+          <div
+            className="text-white tabForecast rounded-2 pb-2 mt-4"
+            style={{ width: "90%", backgroundColor: "#1A4899" }}
+          >
+            <h2>Previsioni per i Prossimi Giorni</h2>
+            <div className="row d-flex fw-bold">
+              <div className="col-2">Giorno</div>
+              <div className="col-2">Descrizione</div>
+              <div className="col-2">Temp Min</div>
+              <div className="col-2">Temp Max</div>
+              <div className="col-2">Umidità</div>
+              <div className="col-2">Vento</div>
+            </div>
+            {daysWeather.map((item, index) => {
+              // Usa moment per formattare la data
+              const date = moment.unix(item.dt).format("dddd D");
+              return (
+                <div className="row d-flex align-items-center" key={index}>
+                  <div className="col-2">{date}</div>
+                  <div className="col-2">
+                    <div className="row d-flex align-items-center">
+                      <div className="col-6 pe-0 text-end">
+                        <img
+                          src={`http://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
+                          alt={item.weather[0].description}
+                          className="weather-img"
+                        />
+                      </div>
+                      <div className="col-6 ps-0 text-start">
+                        {item.weather[0].description}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-2">
+                    {Math.round(item.main.temp_min - 273.15)}°C
+                  </div>
+                  <div className="col-2">
+                    {Math.round(item.main.temp_max - 273.15)}°C
+                  </div>
+                  <div className="col-2">{item.main.humidity}%</div>
+                  <div className="col-2">{item.wind.speed} m/s</div>
+                </div>
+              );
+            })}
+          </div>
         )}
       </div>
     </>
