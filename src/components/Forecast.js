d7da9b2104fd6328519670c3ae8322212be4dc17
Samuele Castaldo <castaldo.samuele@gmail.com>
2024-08-30 11:55:03 +0200

    commit

---
+++ src/components/Forecast.js
@@ -0,0 +1,41 @@
+function Forecast(props) {
+
+    return(
+        <>
+    <div style={{width: "60%"}}>
+        <h2>Previsioni Oggi: {props.forecast.city.name}</h2>
+        <div className="row d-flex fw-bold mb-3">
+          <div className="col-1">Orario</div>
+          <div className="col-3">Icona</div>
+          <div className="col-1">Temp</div>
+          <div className="col-2">Min Temp</div>
+          <div className="col-2">Max Temp</div>
+          <div className="col-1">Umidità</div>
+          <div className="col-2">Vento</div>
+        </div>
+        {props.forecast.list.map((item, index) => {
+          const time = new Date(item.dt * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+          return (
+            <div className="row weather-row d-flex align-items-center" key={index}>
+              <div className="col-1">{time}</div>
+              <div className="col-3">
+                <img
+                  src={`http://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
+                  alt={item.weather[0].description}
+                />
+                {item.weather[0].description}
+              </div>
+              <div className="col-1">{Math.round(item.main.temp - 273.15)}°C</div>
+              <div className="col-2">{Math.round(item.main.temp_min - 273.15)}°C</div>
+              <div className="col-2">{Math.round(item.main.temp_max - 273.15)}°C</div>
+              <div className="col-1">{item.main.humidity}%</div>
+              <div className="col-2">{item.wind.speed} m/s</div>
+            </div>
+          );
+        })}
+
+        </div>
+        </>
+    )
+}
+export default Forecast;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -3,6 +3,7 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import InputAdornment from "@mui/material/InputAdornment";
 import SearchIcon from "@mui/icons-material/Search";
+import Forecast from "../components/Forecast";
 
 function HomePage() {
   const [forecast, setForecast] = useState(null);
@@ -130,53 +131,8 @@ const fetchWeather = (city) => {
           <p>Max Temp: {Math.round(weather.main.temp_max - 273.15)}°C</p>
         </div>
       )}
-       {forecast && (
-        <div>
-            <div className="row">
-                <div className="col">Orario</div>
-                <div className="col">Icona</div>
-                <div className="col">Temp</div>
-                <div className="col">Min Temp</div>
-                <div className="col">Max Temp</div>
-                <div className="col">Umidità</div>
-                <div className="col">Vento</div>
-            </div>
-          <h2>Previsioni Oggi: {forecast.city.name}</h2>
-          <table>
-            <thead>
-              <tr>
-                <th>Orario</th>
-                <th>Icona</th>
-                <th>Temp</th>
-                <th>Min Temp</th>
-                <th>Max Temp</th>
-                <th>Umidità</th>
-                <th>Vento</th>
-              </tr>
-            </thead>
-            <tbody>
-              {forecast.list.map((item, index) => {
-                const time = new Date(item.dt * 1000).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-                return (
-                  <tr key={index}>
-                    <td>{time}</td>
-                    <td>
-                      <img
-                        src={`http://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
-                        alt={item.weather[0].description}
-                      />
-                    </td>
-                    <td>{Math.round(item.main.temp - 273.15)}°C</td>
-                    <td>{Math.round(item.main.temp_min - 273.15)}°C</td>
-                    <td>{Math.round(item.main.temp_max - 273.15)}°C</td>
-                    <td>{item.main.humidity}%</td>
-                    <td>{item.wind.speed} m/s</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
+  {forecast && (
+    <Forecast forecast={forecast}/>
       )}
       </div>
     </>
